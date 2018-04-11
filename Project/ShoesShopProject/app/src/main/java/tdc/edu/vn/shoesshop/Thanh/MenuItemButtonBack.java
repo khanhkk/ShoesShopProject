6887af1360ce742edaa787a3099e5263ae13954b
Lê Nguyễn Việt Thanh <34046664+lenguyenvietthanh@users.noreply.github.com>
2018-04-11 15:18:32 +0700

    update change_informationshop, Frg_fillinfro_patient, product_information,

---
+++ Project/ShoesShopProject/app/src/main/java/tdc/edu/vn/shoesshop/Thanh/MenuItemButtonBack.java
@@ -0,0 +1,11 @@
+package tdc.edu.vn.shoesshop.Thanh;
+
+import android.content.Intent;
+
+/**
+ * Created by Lê Nguyễn Việt Thanh on 005 05/04/2018.
+ */
+
+public class MenuItemButtonBack {
+
+}
---
+++ Project/ShoesShopProject/app/src/main/java/tdc/edu/vn/shoesshop/Thanh/change_informationshop.java
@@ -0,0 +1,123 @@
+package tdc.edu.vn.shoesshop.Thanh;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import tdc.edu.vn.shoesshop.R;
+
+public class change_informationshop extends AppCompatActivity {
+    private static final int CAM_REQUEST = 1313;
+    private Dialog dialog;
+    ImageButton btn_chooseImg,btn_takeaphoto;
+    final int CROP_PIC = 2;
+    private Uri picUri;
+    private Button btn_getimage;
+
+    ImageView img_ava_patient;
+    @Nullable
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_informaition_shop);
+
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog);
+        dialog.setTitle("Choose Avatar Image");
+
+        btn_chooseImg = (ImageButton) dialog.findViewById(R.id.img_choosenGallery);
+        btn_takeaphoto = (ImageButton) dialog.findViewById(R.id.img_choosenTakephoto);
+
+
+        btn_chooseImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseFromGallery();
+            }
+        });
+
+        btn_takeaphoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                profilepictureOnClick();
+            }
+        });
+
+        img_ava_patient = (ImageView) findViewById(R.id.imgView_info);
+        btn_getimage = (Button) findViewById(R.id.btn_infor);
+        btn_getimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(change_informationshop.this
+                        ,"Clicked",Toast.LENGTH_SHORT).show();
+                dialog.show();
+            }
+        });
+    }
+
+
+    public void chooseFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 0);
+    }
+
+
+    private void takeNewProfilePicture(){
+        Activity profileFrag = this;
+        Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        profileFrag.startActivityForResult(cameraintent, CAM_REQUEST);
+    }
+
+
+
+    public void profilepictureOnClick(){
+        takeNewProfilePicture();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == CAM_REQUEST) {
+            if(requestCode == CAM_REQUEST){
+                Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+
+                //  picUri = data.getData();
+
+                img_ava_patient.setImageBitmap(thumbnail);
+                dialog.dismiss();
+            }
+        }
+        else if (resultCode == RESULT_OK){
+            picUri = data.getData();
+
+            // Uri targetUri = data.getData();
+            //  textTargetUri.setText(targetUri.toString());
+            Bitmap bitmap;
+            try {
+                Context applicationContext = dialog.getContext();
+                bitmap = BitmapFactory.decodeStream( applicationContext.getContentResolver().openInputStream(picUri));
+                img_ava_patient.setImageBitmap(bitmap);
+
+                dialog.dismiss();
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
