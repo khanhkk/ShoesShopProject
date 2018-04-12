f02a18af45a68a0da101f1b162fb8a57ac97c791
ngothanhs847 <34046334+ngothanhs847@users.noreply.github.com>
2018-04-13 01:38:08 +0700

    Update OrderInformationForClient
    No errors

---
+++ Project/ShoesShopProject/app/src/main/java/Models/OrderClient.java
@@ -0,0 +1,58 @@
+package Models;
+
+public class OrderClient {
+    private int Hinh;
+    private String Ten;
+    private int Gia;
+    private int GiaGoc;
+    private int soLuong;
+
+
+    public int getHinh() {
+        return Hinh;
+    }
+
+    public void setHinh(int hinh) {
+        Hinh = hinh;
+    }
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String ten) {
+        Ten = ten;
+    }
+
+    public int getGia() {
+        return Gia;
+    }
+
+    public void setGia(int gia) {
+        Gia = gia;
+    }
+
+    public int getGiaGoc() {
+        return GiaGoc;
+    }
+
+    public void setGiaGoc(int giaGoc) {
+        GiaGoc = giaGoc;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public OrderClient(int hinh, String ten, int gia, int giaGoc, int soLuong) {
+        Hinh = hinh;
+        Ten = ten;
+        Gia = gia;
+        GiaGoc = giaGoc;
+        this.soLuong = soLuong;
+    }
+}
