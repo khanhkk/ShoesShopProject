1c0f663501d5eeffcda49dbccf785749633e0460
ngothanhs847 <34046334+ngothanhs847@users.noreply.github.com>
2018-04-17 10:31:51 +0700

    Update host
    dd

--- Project/ShoesShopProject/app/src/main/java/tdc/edu/vn/shoesshop/Son/ClientInformationAfterOrder.java
+++ Project/ShoesShopProject/app/src/main/java/tdc/edu/vn/shoesshop/Son/ClientInformationAfterOrder.java
@@ -1,8 +1,8 @@
 package tdc.edu.vn.shoesshop.Son;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -10,11 +10,12 @@ import tdc.edu.vn.shoesshop.R;
 import tdc.edu.vn.shoesshop.Sang.ListOder;
 
 public class ClientInformationAfterOrder extends AppCompatActivity {
+
     ImageButton back;
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.client_information_after_order_activity);
+
         back = (ImageButton) findViewById(R.id.btnBack);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +24,7 @@ public class ClientInformationAfterOrder extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
