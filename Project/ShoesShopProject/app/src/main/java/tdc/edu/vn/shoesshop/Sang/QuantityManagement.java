0c66a20c9fc5855fd28c361495d981a3351470ba
ngothanhs847 <34046334+ngothanhs847@users.noreply.github.com>
2018-04-17 19:34:58 +0700

    commit link layout 2

--- Project/ShoesShopProject/app/src/main/java/tdc/edu/vn/shoesshop/Sang/QuantityManagement.java
+++ Project/ShoesShopProject/app/src/main/java/tdc/edu/vn/shoesshop/Sang/QuantityManagement.java
@@ -1,15 +1,29 @@
 package tdc.edu.vn.shoesshop.Sang;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import tdc.edu.vn.shoesshop.Khanh.SelectionProductToEditting;
 import tdc.edu.vn.shoesshop.R;
 
 public class QuantityManagement extends AppCompatActivity {
-
+    Button btnSave;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layoutquantitymanagement17);
+        btnSave = (Button) findViewById(R.id.btnSaveQuantityInformation);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(QuantityManagement.this, SelectionProductToEditting.class);
+                startActivity(intent);
+            }
+        });
     }
 }
