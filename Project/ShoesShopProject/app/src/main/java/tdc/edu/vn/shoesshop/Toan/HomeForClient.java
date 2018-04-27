0b3280d96fc8590636f1c06bf290e2902915adc4
ngothanhs847 <34046334+ngothanhs847@users.noreply.github.com>
2018-04-27 17:39:12 +0700

    update Home For Client - Shop
    up

--- Project/ShoesShopProject/app/src/main/java/tdc/edu/vn/shoesshop/Toan/HomeForClient.java
+++ Project/ShoesShopProject/app/src/main/java/tdc/edu/vn/shoesshop/Toan/HomeForClient.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import Controls.General;
 import Controls.TabarControl;
 import tdc.edu.vn.shoesshop.Bao.PersonalOfClientLoginedFragment;
 import tdc.edu.vn.shoesshop.Khanh.Cart;
@@ -22,68 +21,19 @@ import tdc.edu.vn.shoesshop.R;
 import tdc.edu.vn.shoesshop.Son.NotificationFragment;
 
 public class HomeForClient extends AppCompatActivity {
-    /*private FragmentTransaction fragment = null;
-    //private Fragment currentFragment;
-    TabarControl tabarControl;
-
-    NotificationFragment notify = null;
-    Cart cart = null;
-    Home_Client_Fragment b = null;
-    PersonalOfClientLoginedFragment clientLoginedFragment = null; */
-
 
     private FirebaseAuth.AuthStateListener authListener;
-
-
-    //private FirebaseAuth auth;
-
-//TabarControl
-   /* TabarControl.TabarFunctions functions = new TabarControl.TabarFunctions() {
-        @Override
-        public void onButton1Clicked() {
-            fragment = getFragmentManager().beginTransaction();
-            b = new Home_Client_Fragment();
-            fragment.replace(R.id.llParent, b);
-            fragment.commit();
-        }
-
-        @Override
-        public void onButton2Clicked() {
-            fragment = getFragmentManager().beginTransaction();
-            notify = new NotificationFragment();
-            fragment.replace(R.id.llParent, notify);
-            fragment.commit();
-        }
-
-        @Override
-        public void onButton3Clicked() {
-            fragment = getFragmentManager().beginTransaction();
-            cart = new Cart();
-            fragment.replace(R.id.llParent, cart);
-            fragment.commit();
-        }
-
-
-        @Override
-        public void onButton4Clicked() {
-            fragment = getFragmentManager().beginTransaction();
-            clientLoginedFragment = new PersonalOfClientLoginedFragment();
-            fragment.replace(R.id.llParent, clientLoginedFragment);
-            fragment.commit();
-        }
-    };
-
-//    main
-    LinearLayout llContainer; */
+    private FirebaseAuth auth;
+    FirebaseUser users = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_for_client_activity);
-
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
 //        get current user
-        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -109,24 +59,17 @@ public class HomeForClient extends AppCompatActivity {
             }
         };
 
-
-
-     /*  llContainer = (LinearLayout) findViewById(R.id.llParent);
-        tabarControl = (TabarControl) findViewById(R.id.tcTabarClient);
-
-        tabarControl.setImageButton1(R.drawable.home);
-        tabarControl.setImageButton2(R.drawable.notification);
-        tabarControl.setImageButton3(R.drawable.cart);
-        tabarControl.setImageButton4(R.drawable.account);
-        tabarControl.setTabarFunctions(functions);
-        //        default fragment
-        functions.onButton1Clicked();
-*/
-//        data
-
-        //Intent intent = getIntent();
-
     }
+    //                            Intent intent1 = getIntent();
+//                            Bundle bundle1 = intent1.getBundleExtra(LoginActivity.BUNDLE);
+//                            if(bundle1!=null){
+//                                Toast.makeText(getApplicationContext(),"key: "+ bundle1.getString("key"),Toast.LENGTH_LONG).show();
+//                            }
+//                            Bundle bundle = intent1.getBundleExtra("chuyen");
+//                            if (bundle != null) {
+//                                if (bundle.getInt("chuyen") == 1) {
+//                                }
+//                            }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -136,30 +79,23 @@ public class HomeForClient extends AppCompatActivity {
 
                     switch (item.getItemId()) {
                         case R.id.nav_home:
-                            selectedFragment = new HomeLayout();
+                            if (users == null)
+                            {
+                               selectedFragment = new Home_Client_Fragment();
+                            }else
+                                selectedFragment = new Home_User_Fragment();
                             break;
                         case R.id.nav_notification:
                             selectedFragment = new NotificationFragment();
                             break;
                         case R.id.nav_cart:
                             selectedFragment = new Cart();
-                            Intent intent1 = getIntent();
-                            Bundle bundle1 = intent1.getBundleExtra(LoginActivity.BUNDLE);
-                            if(bundle1!=null){
-                                Toast.makeText(getApplicationContext(),"key: "+ bundle1.getString("key"),Toast.LENGTH_LONG).show();
-                            }
-                            Bundle bundle = intent1.getBundleExtra("chuyen");
-                            if (bundle != null) {
-                                if (bundle.getInt("chuyen") == 1) {
-                                }
-                            }
                             break;
                         case R.id.nav_account:
                             selectedFragment = new PersonalOfClientLoginedFragment();
                             break;
                     }
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
-
                     return true;
 
                 }
--- Project/ShoesShopProject/app/src/main/java/tdc/edu/vn/shoesshop/Toan/HomeForShop.java
+++ Project/ShoesShopProject/app/src/main/java/tdc/edu/vn/shoesshop/Toan/HomeForShop.java
@@ -1,81 +1,22 @@
 package tdc.edu.vn.shoesshop.Toan;
 
-import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import Controls.General;
-import Controls.TabarControl;
-import tdc.edu.vn.shoesshop.Bao.PersonalOfClientLoginedFragment;
 import tdc.edu.vn.shoesshop.Bao.PersonalOfShopFragment;
-import tdc.edu.vn.shoesshop.Khanh.Cart;
 import tdc.edu.vn.shoesshop.R;
 import tdc.edu.vn.shoesshop.Sang.TransactionOfShopFragment;
 import tdc.edu.vn.shoesshop.Son.NotificationFragment;
 
 public class HomeForShop extends AppCompatActivity {
 
-   /* private FragmentTransaction fragment;
-    TabarControl tabarControl;
-
-    NotificationFragment notify = null;
-    PersonalOfShopFragment shopFragment = null;
-    TransactionOfShopFragment transaction = null;
-
-    TabarControl.TabarFunctions functions = new TabarControl.TabarFunctions() {
-        @Override
-        public void onButton1Clicked() {
-
-        }
-
-        @Override
-        public void onButton2Clicked() {
-            fragment = getFragmentManager().beginTransaction();
-            transaction = new TransactionOfShopFragment();
-            fragment.replace(R.id.llParentShop, transaction);
-            fragment.commit();
-        }
-
-        @Override
-        public void onButton3Clicked() {
-            fragment = getFragmentManager().beginTransaction();
-            notify = new NotificationFragment();
-            fragment.replace(R.id.llParentShop, notify);
-            fragment.commit();
-        }
-
-        @Override
-        public void onButton4Clicked() {
-            fragment = getFragmentManager().beginTransaction();
-            shopFragment = new PersonalOfShopFragment();
-            fragment.replace(R.id.llParentShop, shopFragment);
-            fragment.commit();
-        }
-    };
-    LinearLayout llContainer; */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_for_shop_activity);
-        // show home shop fragment
-       /* functions.onButton1Clicked();
-
-        llContainer = (LinearLayout) findViewById(R.id.llParent);
-        tabarControl = (TabarControl) findViewById(R.id.tcTabarClient);
-
-        tabarControl.setImageButton1(R.drawable.home);
-        tabarControl.setImageButton2(R.drawable.icon_history);
-        tabarControl.setImageButton3(R.drawable.cart);
-        tabarControl.setImageButton4(R.drawable.account);
-        tabarControl.setTabarFunctions(functions); */
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -85,12 +26,6 @@ public class HomeForShop extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new HomeLayout()).commit();
         }
-
-       /* Intent intent1 = getIntent();
-        Bundle bundle1 = intent1.getBundleExtra(LoginActivity.BUNDLE);
-        if(bundle1!=null){
-            Toast.makeText(getApplicationContext(),"key: "+ bundle1.getString("key"),Toast.LENGTH_LONG).show();
-        }*/
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
